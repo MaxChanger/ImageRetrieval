25a57a3a5c9441857228836d0c217ec97cb8bb7d
MaxChanger <sunjiadai@foxmail.com>
2019-09-21 11:02:55 +0800

    fix html and image

--- retrieval/create_thumb_images.py
+++ retrieval/create_thumb_images.py
@@ -14,18 +14,25 @@ def del_file(path):
 def create_thumb_images(full_folder, thumb_folder, suffix='thumb', height=100, del_former_thumb=False):
     if del_former_thumb:
         del_file(thumb_folder)
+    count = 0
     for image_file in os.listdir(full_folder):
-        image = cv2.imread(full_folder + image_file)
-        height_src, width_src, _ = image.shape
-        #print('width: {}, height: {}'.format(width_src, height_src))
+        try:
+            image = cv2.imread(full_folder + image_file)
+            height_src, width_src, _ = image.shape
+            #print('width: {}, height: {}'.format(width_src, height_src))
 
-        width = (height / height_src) * width_src
-        # print(' Thumb width: {}, height: {}'.format(width, height))
+            width = (height / height_src) * width_src
+            # print(' Thumb width: {}, height: {}'.format(width, height))
 
-        resized_image = cv2.resize(image, (int(width), int(height)))
+            resized_image = cv2.resize(image, (int(width), int(height)))
 
-        image_name, image_extension = os.path.splitext(image_file)
-        cv2.imwrite(thumb_folder + image_name + suffix + image_extension, resized_image)
+            image_name, image_extension = os.path.splitext(image_file)
+            cv2.imwrite(thumb_folder + image_name + suffix + image_extension, resized_image)
+        except Exception as e:
+            count += 1
+            os.remove(full_folder + image_file)
+            print(count,full_folder+image_file, e)
+            # print(e)
     print('Creating thumb images finished.')
 
 
