3bbd55443509aab492604c0be53c8f7faa9a2a9e
Jiadai Sun <sunjiadai@foxmail.com>
2020-01-01 11:45:49 +0800

    clean sth

--- onlytest.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/') # Flask类的route()函数是一个装饰器，它告诉应用程序哪个URL应该调用相关的函数。
-def hello_world():
-   return 'Hello World11122'
-
-if __name__ == '__main__':
-   app.run(host='127.0.0.1', port=8080, debug=True)
